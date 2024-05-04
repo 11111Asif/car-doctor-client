c3684aa4f73fd68d60fc3d190c89f29f0a370cd8
11111Asif <asif040502@gmail.com>
2024-05-04 07:06:21 +0600

    first commit

---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import ServicesCart from "./ServicesCart";
+
+
+const Services = () => {
+    
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    }, [])
+
+    return (
+        <div>
+            <div className="text-center space-y-5 mt-8">
+                <h3 className="text-3xl text-orange-500 font-bold">Services</h3>
+                <h1 className="text-5xl font-bold">Our Service Area</h1>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised<br/> words which don't look even slightly believable. </p>
+
+            </div>
+            <div className="mt-10 grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+               {
+                services.map(service => <ServicesCart key={service._id} service={service}></ServicesCart>)
+               }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
---
+++ src/Pages/Home/Services/ServicesCart.jsx
@@ -0,0 +1,22 @@
+
+import { AiOutlineArrowRight } from "react-icons/ai";
+const ServicesCart = ({service}) => {
+    const {title, price, img} = service
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img src={img} alt="" className="rounded-xl" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-2xl font-bold">{title}</h2>
+          <div className="flex justify-between">
+          <p className="text-orange-600">Price: ${price}</p>
+            <AiOutlineArrowRight className="text-orange-500 text-2xl"></AiOutlineArrowRight>
+          </div>
+         
+        </div>
+      </div>
+    );
+};
+
+export default ServicesCart;
